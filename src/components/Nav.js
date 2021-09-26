dc8a5f07068b5978450fb411da6076f3fb3facd0
jeffseverson2001 <jeff@jefflizseverson.com>
2021-09-26 11:19:46 -0500

    Link Fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -34,12 +34,12 @@ export default function Nav() {
                         <div className="navbar-item nav-strong margin-left has-text-light">Jeff Severson</div>
                     </div>
                     <div className="navbar-end">
-                        <a href="./" className="navbar-item has-text-light">Homepage</a>
-                        <a className="navbar-item has-text-light" href="https://github.com/jeffseverson2001" target="_blank" rel="noopener noreferrer">Github</a>
-                        <a className="navbar-item has-text-light" href="https://www.linkedin.com/in/jeff-severson-880a02208/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                        <a href="./projects" className="navbar-item has-text-light">Projects</a>
-                        <a href="./resume" className="navbar-item has-text-light">Resume</a>
-                        <a href="./contact" className="navbar-item has-text-light">Contact</a>
+                        <link href="./" className="navbar-item has-text-light" />Homepage
+                        <link className="navbar-item has-text-light" href="https://github.com/jeffseverson2001" target="_blank" rel="noopener noreferrer" />Github
+                        <link className="navbar-item has-text-light" href="https://www.linkedin.com/in/jeff-severson-880a02208/" target="_blank" rel="noopener noreferrer" />LinkedIn
+                        <link href="./projects" className="navbar-item has-text-light" />Projects
+                        <link href="./resume" className="navbar-item has-text-light" />Resume
+                        <link href="./contact" className="navbar-item has-text-light" />Contact
                     </div>
                 </div>
             </nav>
